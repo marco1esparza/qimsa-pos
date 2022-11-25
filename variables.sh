2cb4633fd6308ed3920fd5f400c942a52cc98575
Eduardo Baltazar <eduardo.baltazar@jarsa.com.mx>
2022-11-25 18:01:09 +0000

    [ADD] new file variables.sh

---
+++ variables.sh
@@ -0,0 +1,14 @@
+export BASE_IMAGE="vauxoo/odoo-130-image"
+export DOCKER_IMAGE_REPO="quay.io/vauxoo/qimsa"
+export PSQL_VERSION="11"
+export VERSION="13.0"
+export ODOO_REPO="jarsa/odoo"
+export ODOO_BRANCH="13.0"
+export MAIN_APP="qimsa"
+export TRAVIS_PYTHON_VERSION="3.8"
+export TRAVIS_REPO_SLUG="Jarsa/"
+export COUNTRY="MX"
+export CUSTOMER="qimsa"
+export COVERAGE_MIN=20
+export COVERAGE_PRECISION=2
+
