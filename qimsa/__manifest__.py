6727b7deb4b44317dfbbf50ad956f2d627be596d
Ibraim Ferniza <ibraim.ferniza@jarsa.com>
2022-11-25 11:59:23 -0600

    [FIX] qimsa: add qimsa folder and variables.sh file

---
+++ qimsa/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': "Qimsa Instance Module",
+    'summary':  "Module to install Instance",
+    'author': "Jarsa Sistemas",
+    'website': "https://www.jarsa.com.mx",
+    'license': 'LGPL-3',
+    'category': 'Instance',
+    'version': '13.0.1.0.0',
+    'depends': ['base'],
+    'data': [],
+}
